377b5b08771a7257d9b9a9aa9578ed98f59c8697
陈飞龙 <1696741038@qq.com>
2023-01-30 20:51:13 +0800

    策略模式

--- src/design/state/package-info.java
+++ src/design/state/package-info.java
@@ -1,6 +1,6 @@
 /**
  * 包描述：
- * 状态模式（对象模式-行为型模式）
+ * <a href="https://refactoringguru.cn/design-patterns/state">状态模式（对象模式-行为型模式）</a>
  * <pre>
  *     模式说明：允许一个对象在其内部状态改变时改变它的行为。对象看起来似乎修改了它的类。
  * </pre>
